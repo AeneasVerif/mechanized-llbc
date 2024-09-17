7e44dd56e86ed51f39c0c736852d121462158856
Alban <alban.reynaud@laposte.net>
2024-09-17 18:11:50 +0200

    Redefined HLPL+ semantics.

--- src/HLPL_plus.v
+++ src/HLPL_plus.v
@@ -84,12 +84,6 @@ Next Obligation.
 destruct v; destruct w; inversion eq_constructor; inversion eq_subvalues; reflexivity.
 Qed.
 
-Lemma length_1_is_singleton [A : Type] [l : list A] : length l = 1 -> exists a, l = [a].
-Proof.
-  intro H. destruct l as [ | a l'].
-  - inversion H.
-  - exists a. f_equal. apply length_zero_iff_nil. inversion H. auto.
-Qed.
 Next Obligation.
   destruct c; (rewrite length_zero_iff_nil in H; rewrite H) ||
               destruct (length_1_is_singleton H) as [? ->];
@@ -107,17 +101,18 @@ Declare Scope hlpl_plus_scope.
 Delimit Scope hlpl_plus_scope with hlpl_plus.
 
 (* TODO: move in lang.v *)
+(* TODO: set every priority to 0 ?)
 Reserved Notation "'bot'" (at level 50).
 Reserved Notation "'loan^m' l" (at level 50).
 Reserved Notation "'borrow^m' ( l , v )" (at level 50, l at next level, v at next level).
 Reserved Notation "'loc' ( l , v )" (at level 50, l at next level, v at next level).
-Reserved Notation "'ptr' l" (at level 50).
+Reserved Notation "'ptr' ( l )" (at level 50).
 
 Notation "'bot'" := HLPL_plus_bot: hlpl_plus_scope.
 Notation "'loan^m' l" := (HLPL_plus_mut_loan l) : hlpl_plus_scope.
 Notation "'borrow^m' ( l  , v )" := (HLPL_plus_mut_borrow l v) : hlpl_plus_scope.
 Notation "'loc' ( l , v )" := (HLPL_plus_loc l v) : hlpl_plus_scope.
-Notation "'ptr' l" := (HLPL_plus_ptr l) : hlpl_plus_scope.
+Notation "'ptr' ( l )" := (HLPL_plus_ptr l) : hlpl_plus_scope.
 
 (* Bind Scope hlpl_plus_scope with HLPL_plus_val. *)
 Open Scope hlpl_plus_scope.
@@ -126,15 +121,15 @@ Inductive eval_proj (S : HLPL_plus_state) perm : proj -> spath -> spath -> Prop 
 (* Coresponds to R-Deref-MutBorrow and W-Deref-MutBorrow in the article. *)
 | Eval_Deref_MutBorrow q l v
     (imm_or_mut : perm <> Mov)
-    (extract_q : S.[q] = borrow^m(l, v)) :
+    (get_q : S.[q] = borrow^m(l, v)) :
     eval_proj S perm Deref q (q +++ [0])
 (* Coresponds to R-Deref-Ptr-Loc and W-Deref-Ptr-Loc in the article. *)
-| Eval_Deref_Ptr_Locs q q' l w (imm_or_mut : perm <> Mov) :
-    S.[q] = ptr(l) -> sget q' S = loc(l, w) ->
+| Eval_Deref_Ptr_Locs q q' l w (imm_or_mut : perm <> Mov)
+    (get_q : S.[q] = ptr(l)) (get_q' : S.[q'] = loc(l, w)) :
     eval_proj S perm Deref q q'
 (* Coresponds to R-Loc and W-Loc in the article. *)
-| Eval_Loc proj q q' l v (imm_or_mut : perm <> Mov) (extract_q : sget q S = loc(l, v)) :
-    eval_proj S perm proj (q +++ [0]) q' -> eval_proj S perm proj q q
+| Eval_Loc proj q q' l v (imm_or_mut : perm <> Mov) (get_q : sget q S = loc(l, v)) :
+    eval_proj S perm proj (q +++ [0]) q' -> eval_proj S perm proj q q'
 .
 
 (* TODO: eval_path represents a computation, that evaluates and accumulate the result over [...] *)
@@ -144,123 +139,120 @@ Inductive eval_path (S : HLPL_plus_state) perm : path -> spath -> spath -> Prop 
 | Eval_cons proj P p q r: eval_proj S perm proj p q -> eval_path S perm P q r ->
     eval_path S perm (proj :: P) p r.
 
-Lemma eval_path_valid (s : HLPL_plus_state) p perm q r
-  (valid_q : valid_spath s q) (eval_q_r : eval_path s p perm q r) :
+Lemma eval_proj_valid S perm proj q r (H : eval_proj S perm proj q r) : valid_spath S r.
+Proof.
+  induction H.
+  - apply valid_spath_app. split.
+    + apply get_not_bot_valid_spath. rewrite get_q. discriminate.
+    + rewrite get_q. econstructor; reflexivity || constructor.
+  - apply get_not_bot_valid_spath. rewrite get_q'. discriminate.
+  - apply IHeval_proj.
+Qed.
+
+Lemma eval_path_valid (s : HLPL_plus_state) P perm q r
+  (valid_q : valid_spath s q) (eval_q_r : eval_path s perm P q r) :
   valid_spath s r.
 Proof.
   induction eval_q_r.
   - assumption.
-  - apply IHeval_q_r. rewrite valid_spath_app. split; try assumption. rewrite extract_q.
-    constructor. constructor.
-  - auto.
-  - apply IHeval_q_r. rewrite valid_spath_app. split; try assumption. rewrite extract_q.
-    constructor. constructor.
+  - apply IHeval_q_r. eapply eval_proj_valid. eassumption.
 Qed.
 
-(*
-Definition eval_place (s : HLPL_plus_state) (p : place) (perm : permission) (r : spath) :=
-  exists i, find_index s (Var (fst p)) = Some i /\ eval_path s (snd p) perm (i, []) r.
-*)
-Notation eval_place s p perm r :=
-  (exists i, find_index s (Var (fst p)) = Some i /\ eval_path s (snd p) perm (i, []) r).
+Notation eval_place S perm p r :=
+  (exists i, find_binder S (Var (fst p)) = Some i /\ eval_path S perm (snd p) (i, []) r).
 
-Lemma eval_place_valid s p perm r (H : eval_place s p perm r) : valid_spath s r.
+Lemma eval_place_valid s p perm pi (H : eval_place s perm p pi) : valid_spath s pi.
 Proof.
   destruct H as (? & ? & ?). eapply eval_path_valid; try eassumption.
-  eapply find_index_valid_spath. eassumption.
+  eapply find_binder_valid. eassumption.
 Qed.
 
 (* Setting up the definitions for judgements like "loan \notin v" or
    "l is fresh". *)
-Definition state_contains (H : HLPL_plus_val -> Prop) (s : HLPL_plus_state) :=
-  exists p, valid_spath s p /\ H (s{{p}}).
+Definition state_contains (H : HLPL_plus_val -> Prop) (S : HLPL_plus_state) :=
+  exists p, valid_spath S p /\ H (S.[p]).
 
 Definition value_contains (H : HLPL_plus_val -> Prop) (v : HLPL_plus_val) :=
-  exists p, valid_vpath v p /\ H (v{{p}}).
+  exists p, valid_vpath v p /\ H (v.[[p]]).
 
 Definition is_loan (v : HLPL_plus_val) :=
-  exists l, v = vZero (HLPL_plus_loan l).
+  exists l, v = loan^m(l).
 Definition contains_loan := value_contains is_loan.
 
-Definition is_mut_borrow (v : HLPL_plus_val) := exists l w, v = vUnit (HLPL_plus_borrow l) w.
+Definition is_mut_borrow (v : HLPL_plus_val) := exists l w, v = borrow^m(l, w).
 
 Definition contains_outer_loan v :=
-  exists l p, v{{p}} = vZero (HLPL_plus_loan l) /\ (forall q, vprefix q p -> ~is_mut_borrow (v{{q}})).
+  exists l p, v.[[p]] = loan^m(l) /\ (forall q, vprefix q p -> ~is_mut_borrow (v.[[q]])).
 
 Definition contains_outer_loc (v : HLPL_plus_val) :=
-  exists l w p, v{{p}} = vUnit (HLPL_plus_loc l) w /\ (forall q, vprefix q p -> ~is_mut_borrow (v{{q}})).
+  exists l w p, v.[[p]] = loc(l, w) /\ (forall q, vprefix q p -> ~is_mut_borrow (v.[[q]])).
 
-Definition is_loc (v : HLPL_plus_val) :=
-  exists l w, v = vUnit (HLPL_plus_loc l) w.
+Definition is_loc (v : HLPL_plus_val) := exists l w, v = loc(l, w).
 Definition contains_loc := value_contains is_loc.
 
 Variant is_loan_id (l : loan_id) : HLPL_plus_val -> Prop  :=
-| Is_loan_id_loan : is_loan_id l (vZero (HLPL_plus_loan l))
-| Is_loan_id_borrow w : is_loan_id l (vUnit (HLPL_plus_borrow l) w)
-| Is_loan_id_ptr : is_loan_id l (vZero (HLPL_plus_ptr l))
-| Is_loan_id_loc w : is_loan_id l (vUnit (HLPL_plus_loc l) w).
+| Is_loan_id_loan : is_loan_id l (loan^m(l))
+| Is_loan_id_borrow w : is_loan_id l (borrow^m(l, w))
+| Is_loan_id_ptr : is_loan_id l (ptr(l))
+| Is_loan_id_loc w : is_loan_id l (loc(l, w)).
 
-(* TODO: rename "is_fresh" *)
-Definition is_fresh l s := ~state_contains (is_loan_id l) s.
+Definition is_fresh l S := ~state_contains (is_loan_id l) S.
 
-Definition is_borrow (v : HLPL_plus_val) :=
-  exists l w, v = vUnit (HLPL_plus_borrow l) w.
+Definition is_borrow (v : HLPL_plus_val) := exists l w, v = borrow^m(l, w).
 
-Definition not_in_borrow (s : HLPL_plus_state) p :=
-  forall q, prefix q p /\ is_borrow (s{{q}}) -> q = p.
+Definition not_in_borrow (S : HLPL_plus_state) p :=
+  forall q, prefix q p /\ is_borrow (S.[q]) -> q = p.
 
 Definition contains_bot (v : HLPL_plus_val) :=
   value_contains (fun w => w = bot) v.
 
 Inductive copy_val : HLPL_plus_val -> HLPL_plus_val -> Prop :=
-| Copy_val_int (n : nat) : copy_val (vZero (HLPL_plus_int n)) (vZero (HLPL_plus_int n))
-| Copy_ptr l : copy_val (vZero (HLPL_plus_ptr l)) (vZero (HLPL_plus_ptr l))
-| Copy_loc l v w : copy_val v w -> copy_val (vUnit (HLPL_plus_loc l) v) w.
+| Copy_val_int (n : nat) : copy_val (HLPL_plus_int n) (HLPL_plus_int n)
+| Copy_ptr l : copy_val (ptr(l)) (ptr(l))
+| Copy_loc l v w : copy_val v w -> copy_val (loc(l, v)) w.
 
-Variant eval_op : HLPL_plus_state -> operand -> HLPL_plus_val -> HLPL_plus_state -> Prop :=
-| Eval_IntConst s n : eval_op s (IntConst n) (vZero (HLPL_plus_int n)) s
-(* TODO: place should be read in Imm mode. *)
-| Eval_copy s (p : place) q v : eval_place s p Mut q -> copy_val (s{{q}}) v ->
-    eval_op s (Copy p) v s
-| Eval_move s (p : place) q : eval_place s p Mov q -> ~contains_loan (s{{q}}) -> ~contains_bot (s{{q}}) ->
-    eval_op s (Move p) (s{{q}}) (s{{q <- bot}}).
+Variant eval_op (S : HLPL_plus_state) : operand -> HLPL_plus_val -> HLPL_plus_state -> Prop :=
+| Eval_IntConst n : eval_op S (IntConst n) (HLPL_plus_int n) S
+| Eval_copy (p : place) pi v : eval_place S Imm p pi -> copy_val (S.[pi]) v ->
+    eval_op S (Copy p) v S
+| Eval_move (p : place) pi : eval_place S Mov p pi -> ~contains_loan (S.[pi]) -> ~contains_bot (S.[pi]) ->
+  eval_op S (Move p) (S.[pi]) (S.[pi <- bot]).
 
-Variant eval_rvalue : HLPL_plus_state -> rvalue -> HLPL_plus_val -> HLPL_plus_state -> Prop :=
-| Eval_just s op v s' : eval_op s op v s' -> eval_rvalue s (Just op) v s'
+Variant eval_rvalue (S : HLPL_plus_state) : rvalue -> HLPL_plus_val -> HLPL_plus_state -> Prop :=
+| Eval_just op v S' : eval_op S op v S' -> eval_rvalue S (Just op) v S'
 (* For the moment, the only operation is the natural sum. *)
-| Eval_bin_op s s' s'' op_l op_r m n : eval_op s op_l (vZero (HLPL_plus_int m)) s' ->
-   eval_op s' op_r (vZero (HLPL_plus_int n)) s'' ->
-   eval_rvalue s (BinOp op_l op_r) (vZero (HLPL_plus_int (m + n))) s''
-| Eval_pointer_loc s p q l v : eval_place s p Mut q -> s{{q}} = vUnit (HLPL_plus_loc l) v ->
+| Eval_bin_op S' S'' op_l op_r m n : eval_op S op_l (HLPL_plus_int m) S' ->
+   eval_op S' op_r (HLPL_plus_int n) S'' ->
+   eval_rvalue S (BinOp op_l op_r) (HLPL_plus_int (m + n)) S''
+| Eval_pointer_loc p pi l v : eval_place S Mut p pi -> S.[pi] = loc(l, v) ->
     ~contains_loan v -> ~contains_bot v ->
-    eval_rvalue s (&mut p) (vZero (HLPL_plus_ptr l)) s
-| Eval_pointer s p q l : eval_place s p Mut q -> ~contains_loan (s{{q}}) -> ~contains_bot (s{{q}}) ->
-    is_fresh l s -> ~is_loc (s{{q}}) ->
-    eval_rvalue s (&mut p) (vZero (HLPL_plus_ptr l)) (s{{q <- vUnit (HLPL_plus_loc l) (s{{q}})}}).
-Print eval_rvalue.
+    eval_rvalue S (&mut p) (ptr(l)) S
+| Eval_pointer p pi l : eval_place S Mut p pi -> ~contains_loan (S.[pi]) -> ~contains_bot (S.[pi]) ->
+    is_fresh l S -> ~is_loc (S.[pi]) ->
+    eval_rvalue S (&mut p) (ptr(l)) (S.[pi <- loc(l, S.[pi])]).
 
 Inductive reorg : HLPL_plus_state -> HLPL_plus_state -> Prop :=
-| Reorg_refl s : reorg s s
-| Reorg_trans s0 s1 s2 : reorg s0 s1 -> reorg s1 s2 -> reorg s0 s2
-| Reorg_end_borrow_m s (p q : spath) l v : valid_spath s p -> valid_spath s q ->
-    s{{p}} = (vZero (HLPL_plus_loan l)) -> s{{q}} = (vUnit (HLPL_plus_borrow l) v) ->
-    ~contains_loan v -> not_in_borrow s q ->
-    reorg s (s{{p <- v}}{{q <- bot}}).
-Print reorg.
+| Reorg_refl S : reorg S S
+| Reorg_trans S0 S1 S2 : reorg S0 S1 -> reorg S1 S2 -> reorg S0 S2
+| Reorg_end_borrow_m S (p q : spath) l v :
+    S.[p] = loan^m(l) -> S.[q] = borrow^m(l, v) ->
+    ~contains_loan v -> not_in_borrow S q ->
+    reorg S (S.[p <- v].[q <- bot]).
 
 (* When introducing non-terminating features (loops or recursivity), the signature of the relation
    is going to be:
    HLPL_plus_state -> statement -> nat -> Option (statement_result * HLPL_plus_state) -> Prop
 *)
 Inductive eval_stmt : HLPL_plus_state -> statement -> statement_result -> HLPL_plus_state -> Prop :=
-| Eval_nop s : eval_stmt s Nop rUnit s
+| Eval_nop S : eval_stmt S Nop rUnit S
 | Eval_seq_unit S0 S1 S2 stmt_l stmt_r r (eval_stmt_l : eval_stmt S0 stmt_l rUnit S1)
     (eval_stmt_r : eval_stmt S1 stmt_r r S2) : eval_stmt S0 (stmt_l ;; stmt_r) r S2
 | Eval_seq_panic S0 S1 stmt_l stmt_r (eval_stmt_l : eval_stmt S0 stmt_l rPanic S1) :
     eval_stmt S0 (stmt_l ;; stmt_r) rPanic S1
-| Eval_assign S S' p rv v sp : eval_rvalue S rv v S' -> eval_place S' p Mut sp ->
-    ~contains_outer_loc (S'{{sp}}) -> ~contains_outer_loan (S'{{sp}}) ->
-    eval_stmt S (ASSIGN p <- rv) rUnit ((Anon, S'{{sp}}) :: S'{{sp <- v}}).
+| Eval_assign S S' p rv v sp : eval_rvalue S rv v S' -> eval_place S' Mut p sp ->
+    ~contains_outer_loc (S'.[sp]) -> ~contains_outer_loan (S'.[sp]) ->
+    eval_stmt S (ASSIGN p <- rv) rUnit ((Anon, S'.[sp]) :: S'.[sp <- v])
+.
 
 (* TODO: introduce well-formedness judgement. *)
 Variant le_state_base : HLPL_plus_state -> HLPL_plus_state -> Prop :=
