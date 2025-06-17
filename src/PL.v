75e9d03548768356976e9d77d060deec20fd0627
Vincent Rébiscoul <vrebisco@protonmail.com>
2025-06-17 09:39:21 +0200

    mechanized memory of PL with examples

--- src/PL.v
+++ src/PL.v
@@ -21,6 +21,7 @@ Definition address := (block_id * offset)%type.
 
 Inductive PL_val :=
 | PL_bot : PL_val
+| PL_poison : PL_val
 | PL_int : nat -> PL_val 
 | PL_loc : loan_id -> PL_val -> PL_val
 | PL_ptr : loan_id -> PL_val
@@ -32,6 +33,7 @@ Definition pl_val := list PL_val.
 
 Variant PL_nodes :=
 | PL_botC : PL_nodes
+| PL_poisonC: PL_nodes
 | PL_intC : nat -> PL_nodes
 | PL_locC : loan_id -> PL_nodes
 | PL_ptrC : loan_id -> PL_nodes
@@ -44,6 +46,7 @@ Proof. unfold EqDecision, Decision. repeat decide equality. Qed.
 
 Definition PL_arity c := match c with
 | PL_botC => 0
+| PL_poisonC => 0
 | PL_intC _ => 0
 | PL_locC _ => 1
 | PL_ptrC _ => 0
@@ -53,6 +56,7 @@ end.
 
 Definition PL_get_node v := match v with
 | PL_bot => PL_botC
+| PL_poison => PL_poisonC
 | PL_int n => PL_intC n
 | PL_loc l _ => PL_locC l
 | PL_ptr l => PL_ptrC l
@@ -62,6 +66,7 @@ end.
 
 Definition PL_children v := match v with
 | PL_bot => []
+| PL_poison => []
 | PL_int _ => []
 | PL_loc _ v => [v]
 | PL_ptr l => []
@@ -70,6 +75,8 @@ Definition PL_children v := match v with
 end.
 
 Definition PL_fold c vs := match c, vs with
+| PL_botC, _ => PL_bot
+| PL_poisonC, _ => PL_poison
 | PL_intC n, [] => PL_int n
 | PL_locC l, [v] => PL_loc l v
 | PL_ptrC l, [] => PL_ptr l
@@ -116,11 +123,8 @@ Qed.
 Next Obligation. reflexivity. Qed.
 Next Obligation. intros ? []; unfold PL_children; cbn; lia. Qed.
 
-Definition encode_var (x : var) := encode (A := var + block_id) (inl x).
-Definition encode_block_id (bi : block_id) := encode (A := var + block_id) (inr bi).
-
 Record PL_state := {
-  env : Pmap block_id;
+  env : Pmap (block_id * type);
   heap : Pmap pl_val
 }.
 
@@ -160,37 +164,61 @@ Inductive copy_val : PL_val -> PL_val -> Prop :=
   copy_val (PL_pair v1 v2) (PL_pair v1' v2').
 
 (* Functions to lookup and update PL states *)
-Definition lookup_env (S : PL_state) (x : var) : option block_id :=
-  lookup (encode_var x) (env S).
+Definition lookup_block_and_type_env (x : var) (S : PL_state)
+  : option (block_id * type) :=
+  lookup x (env S).
 
-Definition lookup_heap (S : PL_state) (bi : block_id) : option (list PL_val) :=
-  lookup (encode_block_id bi) (heap S).
+Definition lookup_env (x : var) (S : PL_state) : option block_id :=
+  match lookup x (env S) with
+  | None => None
+  | Some (bi, _) => Some bi
+  end.
 
-Definition update_env (S : PL_state) (e : Pmap block_id) :=
+Definition lookup_type_env (x : var) (S : PL_state) : option type :=
+  match lookup x (env S) with
+  | None => None
+  | Some (_, T) => Some T
+  end.
+  
+Definition lookup_heap (bi : block_id) (S : PL_state) : option (list PL_val) :=
+  lookup bi (heap S).
+
+Definition lookup_heap_at_addr (S : PL_state) (addr : address) : option PL_val :=
+  let (bi, off) := addr in
+  match lookup bi (heap S) with
+  | None => None
+  | Some vl =>
+      List.nth_error vl off
+  end.
+
+Definition update_env (S : PL_state) (e : Pmap (block_id * type)) :=
   {|env := e ; heap := heap S |}.
 
 Definition update_heap (S : PL_state) (h : Pmap pl_val) :=
   {|env := env S ; heap := h |}.
   
+Open Scope stdpp_scope.
 
 Inductive read_address (S : PL_state) : place -> type -> address -> Prop :=
-| Read_Addr_Var p t bi x (Hp : snd p = []) (HS : lookup_env S x = Some bi) :
-  read_address S p t (bi, 0)
-| Read_Addr_Deref p t bi n bi' n' vl
-    (Hp : read_address S p t (bi, n))
-    (Hheap : (lookup_heap S bi = Some vl))
+| Read_Addr_Var x t bi
+    (HS : lookup_block_and_type_env x S = Some (bi, t)) :
+  read_address S (x, []) t (bi, 0)
+| Read_Addr_Deref x p t bi n bi' n' vl
+    (Hp : read_address S (x, p) (TRef t) (bi, n))
+    (Hheap : (lookup_heap bi S = Some vl))
     (Hvl : List.nth_error vl n  = Some (PL_address (bi', n'))) :
-  read_address S p t (bi', n')
+  read_address S (x, Deref :: p) t (bi', n')
 | Read_Addr_ProjPairLeft x path t0 t1 bi n
   (H : read_address S (x, path) (TPair t0 t1) (bi, n)) :
     read_address S (x, (Field First) :: path) t0 (bi, n)
 | Read_Addr_ProjPairRight x path t0 t1 bi n
-  (H : read_address S (x, path) (TPair t0 t1) (bi, n + sizeof t0)) :
-  read_address S (x, (Field Second) :: path) t0 (bi, n).
+  (H : read_address S (x, path) (TPair t0 t1) (bi, n)) :
+  read_address S (x, (Field Second) :: path) t1 (bi, n + sizeof t0).
 
 Variant read (S : PL_state) (p : place) (t : type) (vl : pl_val) : Prop :=
   | Read bi n vl'
       (Haddr : read_address S p t (bi, n))
+      (Hheap : Some vl' = lookup_heap bi S)
       (Hsub : vl = firstn (sizeof t) (skipn n vl')) :
     read S p t vl.
 
@@ -198,18 +226,85 @@ Variant write (S : PL_state) (p : place) (t : type) (vl : pl_val)
   : PL_state -> Prop :=
   | Write bi n vl' vl'' h
       (Haddr : read_address S p t (bi, n))
-      (Hlu : Some vl' = lookup_heap S bi)
+      (Hlu : Some vl' = lookup_heap bi S)
       (Hcut : vl'' = (firstn n vl') ++ vl ++ (skipn (n + sizeof t) vl'))
       (Hheap : h = alter (fun _ => vl'') bi (heap S)) :
       write S p t vl (update_heap S h).
 
+Section TestReadWrite.
+Local Open Scope stdpp_scope.
 
 Notation x := 1%positive.
-Definition empty : PL_state := {| env := {[ x := 1%positive ]}; heap := PEmpty |}.
+Notation y := 2%positive.
+Notation b1 := 1%positive.
+Notation b2 := 2%positive.
+Notation b3 := 3%positive.
+Definition pl_state_1 : PL_state :=
+  {|
+    env := {[ x := (b1, TInt) ]};
+    heap := {[ b1 := [PL_poison] ]}
+  |}.
+Definition pl_state_2 : PL_state :=
+  {|
+    env := {[ x := (b1, TPair TInt TInt) ]};
+    heap := {[ b1 := [PL_poison; PL_poison] ]}
+  |}.
+Definition pl_state_3 : PL_state :=
+  {|
+    env := {[ x := (b1, TPair (TRef TInt) TInt) ]};
+    heap := {[ b1 := [PL_address (b1, 1); PL_int 0] ]}
+  |}.
+Definition pl_state_4 : PL_state :=
+  {|
+    env := {[ x := (b1, TRef (TRef TInt)) ]};
+    heap :=
+      {[
+          b1 := [PL_address (b2, 1)] ;
+          b2 := [PL_int 3 ; PL_address (b2, 0)]
+      ]}
+  |}.
+Definition pl_state_5 : PL_state :=
+  {|
+    env := {[ x := (b1, TRef (TRef TInt)) ]};
+    heap :=
+      {[
+          b1 := [PL_address (b2, 1)] ;
+          b2 := [PL_poison ; PL_address (b2, 0)]
+      ]}
+  |}.
 
-Goal exists S, write empty (1%positive, []) TInt [PL_int 0] S.
+Goal exists S, write pl_state_1 (1%positive, []) TInt [PL_int 0] S.
 Proof.
-  eexists. eapply Write.
+  repeat econstructor.
+Qed.
 
-Definition test1 : PL_state := write empty (1%positive, []) TInt [PL_int 0].
+Goal exists S, write pl_state_2 (1%positive, [Field(First)]) TInt [PL_int 0] S.
+Proof.
+  repeat econstructor.
+Qed.
 
+Goal exists S, write pl_state_2 (1%positive, [Field(Second)]) TInt [PL_int 0] S.
+Proof.
+  repeat econstructor.
+Qed.
+
+Goal read pl_state_3 (x, Deref :: [Field(First)]) TInt [PL_int 0].
+Proof.
+  repeat econstructor.
+Qed.
+
+Goal read pl_state_3 (x, [Field(Second)]) TInt [PL_int 0].
+Proof.
+  repeat econstructor.
+Qed.
+
+Goal read pl_state_4 (x, [Deref ; Deref]) TInt [PL_int 3].
+Proof.
+  repeat econstructor.
+Qed.
+
+Goal write pl_state_5 (x, [Deref ; Deref]) TInt [PL_int 3] pl_state_4.
+Proof.
+  repeat econstructor.
+Qed.
+End TestReadWrite.
