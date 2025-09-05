4a3af906cfd659ed43fafab7a2bd824f82547cff
Vincent RÉBISCOUL <vrebisco@protonmail.com>
2025-09-05 16:47:28 +0200

    added reverse induction principle for spath

--- src/ListBackInd.v
+++ src/ListBackInd.v
@@ -1,6 +1,12 @@
 From Stdlib Require Import List.
 From Stdlib Require Import PeanoNat.
+From Stdlib Require Import Lia ZArith.
+From Stdlib Require Import Relations.
 Import ListNotations.
+
+Require Import PathToSubtree.
+Require Import HLPL.
+
 
 Theorem list_back_ind : forall (A : Type) (P : list A -> Prop) (Hnil : P []) (Hrec : (forall (a : A) (l : list A), P l -> P (l ++ [a]))), forall (l : list A), P l.
 Proof.
@@ -27,4 +33,20 @@ Proof.
       specialize (IHlen (rev l') H). specialize (Hrec IHlen).
       assert (H' : rev (b :: l') = rev (rev (a :: l))) by congruence.
       rewrite rev_involutive in H'. rewrite <- H'. simpl. auto.
+Qed.
+
+Theorem state_path_back_ind : forall (P : spath -> Prop) (Hbase : forall enc_x, P (enc_x, [])) (Hrec : (forall (n : nat) (sp : spath), P sp -> P (sp +++ [n]))), forall (sp : spath), P sp.
+Proof.
+  intros P Hbase Hrec sp. rewrite surjective_pairing with (p := sp).
+  remember (snd sp) as vp. generalize dependent sp.
+  induction vp using list_back_ind ; intros sp Hsnd.
+  - apply Hbase.
+  - remember (fst sp, vp) as sp'.
+    rewrite Hsnd, <- surjective_pairing.
+    replace sp with (sp' +++ [a]).
+    * apply Hrec. rewrite surjective_pairing with (p := sp') in Heqsp'.
+      apply base.pair_eq in Heqsp'. destruct Heqsp' as [H1 H2].
+      rewrite surjective_pairing. rewrite <- H2 in IHvp.
+      apply IHvp ; auto.
+    * subst. unfold "+++". simpl. rewrite Hsnd, surjective_pairing. reflexivity.
 Qed.
